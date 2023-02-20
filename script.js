75ace8eb7e2acd93e845596fcc3e8373586b5591
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-02-20 23:57:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+var form = document.querySelector("form");
+var h3 = document.querySelector("h3");
+var nameElement = document.querySelector("#name");
+var yearElement = document.querySelector("#year");
+var btn = document.querySelector("#button");
+ function addValue() {
+	 var h3Value = h3.innerText;
+	 var name = nameElement.value;
+	 var year = yearElement.value;
+	 if(name && year){
+		 h3Value.innerText += "?name" + name + "&year" + year;
+	 
+	 else if( name && !year)
+	 {
+		 h3Value.innerText += "?name"+name;
+	 }
+	 else{
+		 h3Value.innerText += "?year" + year;
+	 }
+ 	h3.textContetnt= h3Value;
+ }
+form.addEventListener("submit", addValue);
